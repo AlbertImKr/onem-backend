c99746ad6be147a80a0c087176f7ee950005c0d7
ALbertIM0427 <guang7226961@naver.com>
2025-03-09 20:31:30 +0900

    refactor: 에러 메시지를 내부로 이동

--- src/main/kotlin/community/whatever/onembackendkotlin/common/exception/UrlNotFoundException.kt
+++ src/main/kotlin/community/whatever/onembackendkotlin/common/exception/UrlNotFoundException.kt
@@ -1,5 +1,7 @@
 package community.whatever.onembackendkotlin.common.exception
 
-const val URL_NOT_FOUND_MESSAGE = "해당 키에 대한 URL이 존재하지 않습니다."
-
-class UrlNotFoundException : RuntimeException(URL_NOT_FOUND_MESSAGE)
+class UrlNotFoundException : RuntimeException(URL_NOT_FOUND_MESSAGE) {
+    companion object {
+        private const val URL_NOT_FOUND_MESSAGE = "해당 키에 대한 URL이 존재하지 않습니다."
+    }
+}
